ff709c60b3036c40d4d7d049a1a44399085cdcf2
Tharaka Nuwan <99677288+IT21208430@users.noreply.github.com>
2022-02-14 18:01:00 +0000

    completed!

--- Tute01.c
+++ Tute01.c
@@ -4,8 +4,22 @@
 
 #include <stdio.h>
 
-int main() {
-  
+int main() 
+{
+  int mark1,mark2; //declaring the variables
+  float avg;
+
+  printf("Enter the marks of subject 1:");//input mark1
+  scanf("%d",&mark1);
+
+  printf("Enter the marks of subject 2:");//input mark2
+  scanf("%d",&mark2);
+
+  avg=(mark1+mark2)/2;//calculation
+
+  printf("Average :%.2f\n",avg); //displaying the average
+
+   
   return 0;
 }
 
