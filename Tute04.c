c2329bd152147adf2eaeef4e7831e921f5156758
Tharaka Nuwan <99677288+IT21208430@users.noreply.github.com>
2022-02-16 21:08:19 +0000

    completed!

--- Tute04.c
+++ Tute04.c
@@ -6,14 +6,45 @@ Do not change the code given in the main() function when you are implementing yo
 
 #include <stdio.h>
 
+int maximum(int n1 , int n2); //declaring the function variables
+int minimum(int n1 , int n2);
+int multiply(int n1 , int n2);
+
 int main() {
-   int no1, no2;
-   printf("Enter a value for no 1 : ");
+   int no1, no2; //declaring the variables
+   printf("Enter a value for no 1 : "); //user input
    scanf("%d", &no1);
-   printf("Enter a value for no 2 : ");
+   printf("Enter a value for no 2 : "); //user input
    scanf("%d", &no2);
-   printf("%d ", minimum(no1, no2));
-   printf("%d ", maximum(no1, no2));
-   printf("%d ", multiply(no1, no2));
+   printf("%d ", minimum(no1, no2)); //displaying the minimum
+   printf("%d ", maximum(no1, no2)); //displaying the maximum
+   printf("%d ", multiply(no1, no2)); // displaying the product
    return 0;
+}
+
+int maximum(int n1 ,int n2) //finding the maximum
+{
+  if(n1 <= n2)
+  {
+    return n2;
+  }
+  else
+    return n1;
+
+}
+
+int minimum(int n1 ,int n2) //finding the minimum
+{
+  if(n1 <= n2)
+  {
+    return n1;
+  }
+  else
+    return n2;
+
+}
+
+int multiply(int n1 ; int n2) // multiplication
+{
+  return n1 * n2 ;
 }
